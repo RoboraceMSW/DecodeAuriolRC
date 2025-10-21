304fcc2ede03dd95f4d465f5b643453bb43ae897
RoboRace <roborace@sirious.nl>
2025-10-22 01:15:38 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,55 +1,67 @@
 IR.IR_callbackUser(function () {
-    if (IR.IR_read() == 23) {
+    if (IR.IR_read() == 93) {
         basic.showString("A1")
     }
-    if (IR.IR_read() == 63) {
+    if (IR.IR_read() == 157) {
+        basic.showString("B1")
+    }    
+    if (IR.IR_read() == 29) {
         basic.showString("C1")
     }
-    if (IR.IR_read() == 4) {
+    if (IR.IR_read() == 221) {
         basic.showString("A2")
     }
-    if (IR.IR_read() == 6) {
+    if (IR.IR_read() == 253) {
         basic.showString("B2")
     }
-    if (IR.IR_read() == 31) {
+    if (IR.IR_read() == 61) {
         basic.showString("C2")
     }
-    if (IR.IR_read() == 8) {
+    if (IR.IR_read() == 31) {
         basic.showString("A3")
     }
-    if (IR.IR_read() == 10) {
+    if (IR.IR_read() == 21) {
         basic.showString("B3")
     }
-    if (IR.IR_read() == 47) {
+    if (IR.IR_read() == 9) {
         basic.showString("C3")
     }
-    if (IR.IR_read() == 12) {
+    if (IR.IR_read() == 22) {
         basic.showString("A4")
     }
-    if (IR.IR_read() == 14) {
+    if (IR.IR_read() == 25) {
         basic.showString("B4")
     }
-    if (IR.IR_read() == 15) {
+    if (IR.IR_read() == 13) {
         basic.showString("C4")
     }
-    if (IR.IR_read() == 16) {
+    if (IR.IR_read() == 12) {
         basic.showString("A5")
     }
-    if (IR.IR_read() == 17) {
+    if (IR.IR_read() == 24) {
         basic.showString("B5")
     }
-    if (IR.IR_read() == 55) {
+    if (IR.IR_read() == 133) {
         basic.showString("C5")
     }
-    if (IR.IR_read() == 20) {
+    if (IR.IR_read() == 8) {
         basic.showString("A6")
     }
-    if (IR.IR_read() == 21) {
+    if (IR.IR_read() == 199) {
         basic.showString("B6")
     }
-    if (IR.IR_read() == 22) {
+    if (IR.IR_read() == 165) {
         basic.showString("C6")
     }
+    if (IR.IR_read() == 189) {
+        basic.showString("A7")
+    }
+    if (IR.IR_read() == 181) {
+        basic.showString("B7")
+    }
+    if (IR.IR_read() == 173) {
+        basic.showString("C7")
+    }    
 })
 Maqueen_V5.I2CInit()
 IR.IR_init()
